36187295c2afc4e964388c7653cf3cfc4f56bfb5
arrowmin <botbot_aromin@yahoo.com>
2024-10-23 01:45:07 -0700

    Finish Exercise 1

--- src/components/Expense/ExpenseItem.tsx
+++ src/components/Expense/ExpenseItem.tsx
@@ -3,17 +3,15 @@ import { useContext } from "react";
 import { AppContext } from "../../context/AppContext";
 const ExpenseItem = (currentExpense: Expense) => {
   // Exercise: Consume the AppContext here
-  /* */
   const { expenses, setExpenses } = useContext(AppContext);
   const handleDeleteExpense = (currentExpense: Expense) => {
     // Exercise: Remove expense from expenses context array
-    /*
+
     const updatedExpenses = expenses.filter(
-      (expense) => expense !== currentExpense
+      (expense) => expense.id !== currentExpense.id
     );
     setExpenses(updatedExpenses);
     console.log(expenses);
-    */
   };
 
   return (
